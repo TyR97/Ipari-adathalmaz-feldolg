8d4b51c37ab7128d55086430d176a4538d4ba028
TyR97 <vargajanos97@gmail.com>
2024-10-27 19:26:11 +0100

    refactor: move DataFrame related  functions into dedicated and separate file

---
+++ DataFrameProcessing.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+"""
+ Reads CSV into a Pandas DataFrame.
+
+  Parameters
+  ----------
+  csv_path : string
+      Path to csv file
+      
+  Returns
+  -------
+  Pandas DataFrame
+  """
+def df_init(csv_path):
+    try:
+        df = pd.read_csv(csv_path, sep=';', engine='python')
+        print('CSV loaded successfully.')
+        return df
+    except Exception as err:
+        print(err)
+
+
+"""
+ Iterates trough the DataFrame columns, creates a new Pandas DataFrame with the following columns:
+    MeresID
+    Panel
+    Ido
+    Homerseklet
+
+  Parameters
+  ----------
+  old_df : DataFrame
+      The DataFrame to be copied
+
+  Returns
+  -------
+  Pandas DataFrame
+  """
+def df_processor(old_df):
+    new_df = pd.DataFrame(columns=['MeresID', 'Panel', 'Ido', 'Homerseklet'])
+
+    for idx in range(len(old_df)):
+        time_col = f'Panel hőfok {idx} [°C] Time'
+        temp_col = f'Panel hőfok {idx} [°C] ValueY'
+
+        if temp_col in old_df.columns and time_col in old_df.columns:
+            temp_df = pd.DataFrame({
+                'MeresID': old_df.index + 1,
+                'Panel': idx,
+                'Ido': old_df[time_col],
+                'Homerseklet': old_df[temp_col]
+            })
+            new_df = pd.concat([new_df, temp_df])
+    return new_df
