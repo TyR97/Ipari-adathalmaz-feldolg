91e411699e00a6d8c056be06d488774c2fc81fc6
TyR97 <vargajanos97@gmail.com>
2024-11-02 15:37:39 +0100

    feat: add time concat and drop columns

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ import DB_Commands as DBC
 
 conn = DBC.db_init('test3.db')
 
-meres_df = DFP.df_init('Hűtőpanelek.csv')
-new_df = DFP.df_processor(meres_df)
+meres_df = DFP.df_processor(DFP.df_init('Hűtőpanelek.csv'))
+
 adag_df = DFP.df_init('Adagok.csv')
 
 adag_df['Kezdet_DATUM'] =  DFP.date_to_right_format(adag_df['Kezdet_DATUM'])
 adag_df['Vege_DATUM'] = DFP.date_to_right_format(adag_df['Vege_DATUM'])
+adag_df['KezdetIdo'] = adag_df.apply(lambda row: f"{row['Kezdet_DATUM']} {row['Kezdet_IDO']}", axis=1)
+adag_df['VegeIdo'] = adag_df.apply(lambda row: f"{row['Vege_DATUM']} {row['Vege_IDO']}", axis=1)
+adag_df = adag_df.drop(['Kezdet_DATUM', 'Vege_DATUM', 'Kezdet_IDO', 'Vege_IDO', 'ADAGKOZI IDO', 'ADAGIDO'], axis=1)
+
+DFP.df_cleaner(meres_df)
+
+print(adag_df)
+#print(adag_df['VegeIdo'])
 
 
-DFP.df_cleaner(new_df)
+#DBC.db_export(conn, meres_df, adag_df)
 
-#print(adag_df)
-
-DBC.db_export(conn, new_df, adag_df)
-#print(adag_df['Vege_DATUM'])
-#print(new_df)
 
 
 
